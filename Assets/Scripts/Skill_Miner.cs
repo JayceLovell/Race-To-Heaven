7e190d5d448c928610ebc2f3351555588c824491
micheal sun <michealzun@gmail.com>
2018-11-19 23:21:10 -0500

    Add files via upload

---
+++ Assets/Scripts/Skill_Miner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_Miner : MonoBehaviour {
+    public float staminaCost;
+    public float staminaRegen;
+    public GameObject slashPrefab;
+    public float cooldown; // must be more than 1, since our previous slash gameobject takes 1 sec to despawn
+
+    float maxStamina = 100;
+    float currStamina;
+    float currCD = 0;
+    GameObject slash = null;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        currStamina = maxStamina;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        if (currStamina < maxStamina) currStamina += staminaRegen;
+        else if (currStamina > maxStamina) currStamina = maxStamina;
+
+        if (currCD > 0) currCD -= Time.deltaTime;
+        else if (currCD < 0) currCD = 0;
+
+
+        if (currStamina > staminaCost && Input.GetButtonDown("skill") && currCD == 0)
+        {
+            currStamina -= staminaCost;
+            currCD = cooldown;
+            slash = Instantiate(slashPrefab, this.transform.position+ new Vector3(2,0,0), Quaternion.identity);
+            StartCoroutine(Timer(1));
+        }
+
+    }
+
+    IEnumerator Timer(int counter)
+    {
+        yield return new WaitForSeconds(counter);
+        Destroy(slash);
+    }
+}
---
+++ Assets/Scripts/Skill_Placeholder.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_Placeholder : MonoBehaviour {
+    public float staminaCost;
+    public float staminaRegen;
+    public float cooldown;
+    public GameObject portalPrefab;
+   
+    float maxStamina = 100;
+    float currStamina;
+    float currCD = 0;
+    GameObject portal = null;
+    Rigidbody2D rb;
+    // Use this for initialization
+    void Start()
+    {
+        currStamina = maxStamina;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currStamina < maxStamina) currStamina += staminaRegen;
+        else if (currStamina > maxStamina) currStamina = maxStamina;
+
+        if (currCD > 0) currCD -= Time.deltaTime;
+        else if (currCD < 0) currCD = 0;
+
+
+        if (currStamina > staminaCost && Input.GetButtonDown("skill") && currCD == 0)
+        {
+            if (portal == null)
+            {
+                currStamina -= staminaCost;
+                portal = Instantiate(portalPrefab, this.transform.position, Quaternion.identity);
+            }
+            else
+            {
+                currCD = cooldown;
+                this.transform.position = portal.transform.position;
+                Destroy(portal);   
+            }
+        }
+    }
+}
