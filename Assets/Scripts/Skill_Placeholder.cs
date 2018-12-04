216e07d2ec7640abad3623e36df53b15587d1646
micheal sun <michealzun@gmail.com>
2018-12-04 17:53:52 -0500

    placeholder

--- Assets/Scripts/Skill_Placeholder.cs
+++ Assets/Scripts/Skill_Placeholder.cs
@@ -4,47 +4,35 @@ using UnityEngine;
 
 public class Skill_Placeholder : MonoBehaviour {
     public float staminaCost;
-    public float staminaRegen;
-    public float cooldown;
     public GameObject portalPrefab;
-   
-    float maxStamina = 100;
+    private SkillBarScript _skillbar;
     float currStamina;
-    float currCD = 0;
     GameObject portal = null;
     Rigidbody2D rb;
     // Use this for initialization
     void Start()
     {
-        currStamina = maxStamina;
+        _skillbar = GetComponent<SkillBarScript>();
+        currStamina = _skillbar.StaminaAmount;
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currStamina < maxStamina) currStamina += staminaRegen;
-        else if (currStamina > maxStamina) currStamina = maxStamina;
 
-        if (currCD > 0) currCD -= Time.deltaTime;
-        else if (currCD < 0) currCD = 0;
+        currStamina = _skillbar.StaminaAmount;
 
-
-        if (currStamina > staminaCost && Input.GetButtonDown("skill") && currCD == 0)
+        if (currStamina > staminaCost && Input.GetButtonDown("Skill") && portal == null)
         {
-            if (portal == null)
-            {
-                currStamina -= staminaCost;
+                _skillbar.StaminaAmount -= staminaCost;
                 portal = Instantiate(portalPrefab, this.transform.position, Quaternion.identity);
-            }
-            else
-            {
-                currCD = cooldown;
-                this.transform.position = portal.transform.position;
-                Destroy(portal);   
-            }
         }
-
+        else if ( Input.GetButtonDown("Skill") && portal != null)
+        {
+            this.transform.position = portal.transform.position;
+            Destroy(portal);
+        }
        
     }
     private void OnCollisionStay2D(Collision2D collision)
